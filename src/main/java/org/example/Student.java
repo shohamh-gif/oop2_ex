e53df70f947840cbd04b8df434462cb0c396f02a
Owner <shohamh@edu.aac.ac.il>
2026-06-25 17:14:15 +0300

    second ex

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -34,7 +34,7 @@ public class Student implements Comparable<Student> {
     }
 
     public int countOfCredits() {
-        if (this.courses == null || this.courses.size() <= 0) {
+        if (this.courses == null || this.courses.isEmpty()) {
             return 0;
         }
         return this.courses
